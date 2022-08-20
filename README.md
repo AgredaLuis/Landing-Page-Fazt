540e1ba1289c8c9cdb5440794c54b0579ba7aa5b
AgredaLuis <luis.agreda98@gmail.com>
2022-08-20 15:58:57 -0400

    commit del landing page terminado

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+# Practica de Landing Page , utilizando flexbox y Grid en el CSS , con un simple toggle en JS
+
+
+# Recomendations
+* Minimize the size of Images
+
+
+# Resources
+* [Pexels.com](https://www.pexels.com/)
+* [HeroPatterns.com](https://www.heropatterns.com/). Death Start is choosen in this project
+* [Creator content] (https://www.youtube.com/c/FaztTech)
---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Slide bar ///
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay:500});
+ScrollReveal().reveal('.cards-banner-ones', {delay:500});
+ScrollReveal().reveal('.cards-banner-two', {delay:500});
